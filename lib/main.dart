7a2fb90ec1ac711f4b438ce42f05143037100817
xeu100 <liamryde@gmail.com>
2019-08-04 10:17:55 -0400

    removed unused libs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:fchs_robotics/pages/HomePage.dart';
-import 'package:fchs_robotics/utilities/Firebase.dart';
 import 'package:flutter_web/material.dart';
 
 void main() {
--- lib/utilities/Firebase.dart
+++ lib/utilities/Firebase.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
-
 import 'package:firebase/firebase.dart';
 
 class Firebase {
-
-  //TODO: Add authentication stuff
 
   static App app = initializeApp(
     apiKey: "AIzaSyBZk0RC3_xmTKms2_2KzWUDyPMdBhbLehQ",
@@ -14,14 +10,4 @@ class Firebase {
     storageBucket: "",
     messagingSenderId: "163368391726",
   );
-
-  Future<UserCredential> authenticateUser() async {
-    if(!window.localStorage.containsKey('email') && !window.localStorage.containsKey('password')) {
-      return null;
-    }
-    var email = window.localStorage['email'];
-    var password = window.localStorage['password'];
-    UserCredential cred = await app.auth().signInWithEmailAndPassword(email, password);
-    return cred;
-  }
 }
