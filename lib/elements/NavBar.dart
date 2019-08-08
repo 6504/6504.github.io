4897b4e74ee7663e67fe9343c714652ba69b16c5
xeu100 <liamryde@gmail.com>
2019-08-08 12:20:33 -0400

    Added NavBar mobile view

--- lib/elements/NavBar.dart
+++ lib/elements/NavBar.dart
@@ -5,9 +5,6 @@ import 'package:flutter_web/material.dart';
 
 class NavBar extends StatefulWidget {
 
-  final TextStyle _navBarStyle = getTextStyle().copyWith(color: Colors.white, fontWeight: FontWeight.w100, fontSize: 25.0);
-  TextStyle _coreStyle;
-
   @override
   State<StatefulWidget> createState() {
     return NavBarState();
@@ -19,99 +16,138 @@ class NavBarState extends State<NavBar> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    widget._coreStyle = getTextStyle().copyWith(color: Colors.white, fontSize: MediaQuery.of(context).size.height*0.04, fontFamily: 'ConcertOne');
     return Stack(
       children: <Widget>[
         Container(
-          width: MediaQuery.of(context).size.width,
-          height: 110.0,
-          decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Color.fromRGBO(8, 87, 33, 1.0)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            width: MediaQuery.of(context).size.width,
+            height: 60.0,
+            decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Color.fromRGBO(8, 87, 33, 1.0),),
+            child: Align(
+              alignment: Alignment.center,
+              child: Wrap(
+                spacing: 170.0,
                 children: <Widget>[
-                  Wrap(
-                    spacing: 260.0,
-                    children: <Widget>[
-                      Text("FALLS CHURCH", style: widget._coreStyle,),
-                      Text("ROBOTICS CLUB", style: widget._coreStyle,)
-                    ],
-                  ),
+                  Text("FALLS CHURCH HIGH SCHOOL ROBOTICS CLUB", style: getTextStyle().copyWith(color: Colors.white, fontSize: 20.0, fontFamily: 'ConcertOne')),
                 ],
-              )
-            ],
-          ),
+              ),
+            )
         ),
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: 65.0,
-          decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Colors.green),
-          child: ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: <Widget>[
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("HOME", style: widget._navBarStyle,),
-              ),
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("ABOUT US", style: widget._navBarStyle,),
-              ),
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("TEAM", style: widget._navBarStyle,),
-              ),
-              Padding(padding: EdgeInsets.symmetric(horizontal: 130.0)),
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("ROBOTS", style: widget._navBarStyle,),
-              ),
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("SPONSORS", style: widget._navBarStyle,),
-              ),
-              MaterialButton(
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }))
-                },
-                child: Text("MORE", style: widget._navBarStyle,),
-              ),
-            ],
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.width >= 850 ? 60.0 : 120.0,
+            decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Color.fromRGBO(44, 119, 68, 1.0),),
+            child: MediaQuery.of(context).size.width >= 850 ? LargeNavBarElement() : SmallNavBarElement(),
           ),
         ),
         Align(
-          alignment: Alignment.topCenter,
+          alignment: Alignment.bottomCenter,
           child: Container(
-            alignment: Alignment.topCenter,
-            width: MediaQuery.of(context).size.width*0.1,
-            height: MediaQuery.of(context).size.height*0.13,
-            decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Colors.green),
-            child: Image.network('assets/images/main_logo.png'),
+            width: 130,
+            height: MediaQuery.of(context).size.width >= 850 ? 130 : 0,
+            decoration: BoxDecoration(boxShadow: [BoxShadow(blurRadius: 5.0)], color: Color.fromRGBO(44, 119, 68, 1.0), borderRadius: BorderRadius.vertical(top: Radius.circular(10.0)), image: DecorationImage(image: AssetImage('images/main_logo.png'))),
           ),
+        ),
+      ],
+    );
+  }
+
+}
+
+class LargeNavBarElement extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 20.0,
+      alignment: WrapAlignment.center,
+      children: <Widget>[
+        MaterialButton(
+          onPressed: () => {},
+          child: Text("Home", style: getTextStyle()),
+          color: Colors.white
+        ),
+        MaterialButton(
+          onPressed: () => {},
+          child: Text("About", style: getTextStyle(),),
+          color: Colors.white
+        ),
+        MaterialButton(
+            onPressed: () => {},
+            child: Text("Team", style: getTextStyle(),),
+            color: Colors.white
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 70.0),
+        ),
+        MaterialButton(
+            onPressed: () => {},
+            child: Text("Robots", style: getTextStyle(),),
+            color: Colors.white
+        ),
+        MaterialButton(
+            onPressed: () => {},
+            child: Text("Sponors", style: getTextStyle(),),
+            color: Colors.white
+        ),
+        MaterialButton(
+            onPressed: () => {},
+            child: Text("More", style: getTextStyle(),),
+            color: Colors.white
+        ),
+      ],
+    );
+  }
+
+}
+
+class SmallNavBarElement extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Wrap(
+          spacing: 20.0,
+          children: <Widget>[
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("Home", style: getTextStyle()),
+                color: Colors.white
+            ),
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("About", style: getTextStyle(),),
+                color: Colors.white
+            ),
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("Team", style: getTextStyle(),),
+                color: Colors.white
+            ),
+          ],
+        ),
+        Wrap(
+          spacing: 20.0,
+          children: <Widget>[
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("Robots", style: getTextStyle(),),
+                color: Colors.white
+            ),
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("Sponors", style: getTextStyle(),),
+                color: Colors.white
+            ),
+            MaterialButton(
+                onPressed: () => {},
+                child: Text("More", style: getTextStyle(),),
+                color: Colors.white
+            ),
+          ],
         )
       ],
     );
