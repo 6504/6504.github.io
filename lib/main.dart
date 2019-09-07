1d676f4c523e1ef44678424b8d988c28a4b0d6ae
xeu100 <liamryde@gmail.com>
2019-09-07 15:47:12 -0400

    Added url functions to pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
-import 'package:fchs_robotics/pages/HomePage.dart';
 import 'package:flutter_web/material.dart';
+import 'dart:html';
+
+import 'package:fchs_robotics/pages/HomePage.dart';
+import 'package:fchs_robotics/pages/AboutPage.dart';
+import 'package:fchs_robotics/pages/TeamPage.dart';
+
 
 void main() {
   runApp(
     MaterialApp(
-      home: HomePage(),
+      home: getPage(window.location.hash.substring(1)),
     )
   );
+}
+
+Widget getPage(hash) {
+  if(hash == "/about") {
+    return AboutPage();
+  } else if(hash == "/team") {
+    return TeamPage();
+  } else {
+    return HomePage();
+  }
 }
