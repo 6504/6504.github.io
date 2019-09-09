16ecef96eec0cf402083ac15317ce1fdf0a168af
xeu100 <liamryde@gmail.com>
2019-09-08 20:52:30 -0400

    did someone say login

--- lib/pages/portal/LoginPage.dart
+++ lib/pages/portal/LoginPage.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:fchs_robotics/elements/NavBar.dart';
+import 'package:fchs_robotics/pages/portal/DashboardPage.dart';
 import 'package:fchs_robotics/utilities/Defaults.dart';
 import 'package:flutter/material.dart';
 
@@ -21,7 +24,7 @@ class LoginPage extends StatelessWidget {
                 Padding(padding: EdgeInsets.only(top: 60.0),),
                 Text('Welcome! Please login...', style: getTextStyle().copyWith(fontSize: 20.0), textAlign: TextAlign.center,),
                 Card(
-                  margin: EdgeInsets.all(100.0).copyWith(left: 300.00).copyWith(right: 300.0),
+                  margin: EdgeInsets.all(100.0).copyWith(left: MediaQuery.of(context).size.width >= 850 ? 300 : 0).copyWith(right: MediaQuery.of(context).size.width >= 850 ? 300 : 0),
                   child: Padding(
                     padding: EdgeInsets.all(20.0),
                     child: Column(
@@ -36,16 +39,18 @@ class LoginPage extends StatelessWidget {
                         TextField(
                           controller: _password,
                           enableInteractiveSelection: true,
+                          obscureText: true,
                           keyboardType: TextInputType.text,
                           decoration: InputDecoration(hintText: "Password", hintStyle: getTextStyle()),
                           style: getTextStyle(),
                         ),
                         Row(
-
                           children: <Widget>[
                             MaterialButton(
                               onPressed: () => {
-                                print(_email.text)
+                                window.localStorage['email'] = _email.text,
+                                window.localStorage['password'] = _password.text,
+                                Navigator.push(context, MaterialPageRoute(builder: (context) {return DashboardPage();}))
                               },
                               child: Text("LOGIN", style: getTextStyle().copyWith(color: Colors.white),),
                               color: Colors.blue,
@@ -53,7 +58,7 @@ class LoginPage extends StatelessWidget {
                             Padding(padding: EdgeInsets.symmetric(horizontal: 10.0),),
                             MaterialButton(
                               onPressed: () => {
-                                print(_email.text)
+
                               },
                               child: Text("REGISTER", style: getTextStyle().copyWith(color: Colors.white),),
                               color: Colors.orangeAccent,
