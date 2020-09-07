bd10f19de053a2c1fb2fdb868e9838836e099251
Liam Ryde <liamryde@gmail.com>
2020-09-06 21:27:24 -0400

    Added logging in

--- lib/elements/ScaffoldWrapper.dart
+++ lib/elements/ScaffoldWrapper.dart
@@ -2,13 +2,14 @@
 import 'package:fallschurchrobotics/pages/About/About.dart';
 import 'package:fallschurchrobotics/pages/Home.dart';
 import 'package:fallschurchrobotics/utilities/LocalStorageManager.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 class ScaffoldWrapper extends StatefulWidget {
   static int _currentIndex = 0;
   Widget _page;
+  User _user;
 
   @override
   State<StatefulWidget> createState() {
@@ -25,8 +26,14 @@ class ScaffoldWrapper extends StatefulWidget {
 
 class ScaffoldWrapperState extends State<ScaffoldWrapper> {
 
+  TextEditingController _email = TextEditingController(text: "");
+  TextEditingController _password = TextEditingController(text: "");
+
+
   @override
   Widget build(BuildContext context) {
+    BuildContext scaffoldContext;
+    checkAuthentication();
     return Scaffold(
       appBar: AppBar(
         title: Text("Falls Church High School Robotics Club"),
@@ -38,10 +45,108 @@ class ScaffoldWrapperState extends State<ScaffoldWrapper> {
       ),
       drawer: Builder(
         builder: (BuildContext context) {
+          scaffoldContext = context;
           return Drawer(
             child: ListView(
               shrinkWrap: true,
               children: [
+                Divider(),
+                Center(child: Text("Account Information", style: TextStyle(fontSize: 20),)),
+                Divider(),
+                Builder(builder: (context) {
+                  if(widget._user == null) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MaterialButton(onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (context) {
+                              return AlertDialog(
+                                title: Text("Enter Your Credentials"),
+                                content: Form(
+                                  child: Column(
+                                    children: [
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Enter your email',
+                                        ),
+                                        controller: _email,
+                                      ),
+                                      TextFormField(
+                                        decoration: InputDecoration(
+                                          labelText: 'Enter your password',
+                                        ),
+                                        obscureText: true,
+                                        controller: _password,
+                                      ),
+                                      MaterialButton(onPressed: () {
+                                        FirebaseAuth.instance.signInWithEmailAndPassword(email: _email.text, password: _password.text).then((value) => checkAuthentication()).catchError((error) => {
+                                          if(error.code == 'user-not-found') {
+                                            Navigator.pop(scaffoldContext),
+                                            Scaffold.of(scaffoldContext).showSnackBar(SnackBar(content: Text("The credentials you entered don't exist or are invalid.")))
+                                          } else if(error.code == 'wrong-password') {
+                                            Navigator.pop(scaffoldContext),
+                                            Scaffold.of(scaffoldContext).showSnackBar(SnackBar(content: Text("The credentials you entered don't exist or are invalid.")))
+                                          } else {
+                                            Navigator.pop(scaffoldContext),
+                                            Scaffold.of(scaffoldContext).showSnackBar(SnackBar(content: Text("An unexpected authentication error occurred.")))
+                                          }
+                                        });
+                                        Navigator.pop(context);
+                                      }, child: Text("Login"),)
+                                    ],
+                                  ),
+                                  autovalidate: true,
+                                ),
+                              );
+                            }
+                          );
+                        }, child: Text("Login"),),
+                        MaterialButton(onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text("Enter Your Email"),
+                                  content: Form(
+                                    child: Column(
+                                      children: [
+                                        TextFormField(
+                                          decoration: InputDecoration(
+                                            labelText: 'Enter your email',
+                                          ),
+                                          controller: _email,
+                                        ),
+                                        MaterialButton(onPressed: () {
+                                          FirebaseAuth.instance.sendPasswordResetEmail(email: _email.text);
+                                          Scaffold.of(scaffoldContext).showSnackBar(SnackBar(content: Text("If that email exists, a password reset request was sent.")));
+                                          Navigator.pop(context);
+                                        }, child: Text("Request Password Reset"),)
+                                      ],
+                                    ),
+                                    autovalidate: true,
+                                  ),
+                                );
+                              }
+                          );
+                        }, child: Text("Reset Password"),),
+                      ],
+                    );
+                  } else {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        MaterialButton(onPressed: () {
+                          FirebaseAuth.instance.signOut().then((value) => checkAuthentication());
+                        }, child: Text("Logout"),),
+                        MaterialButton(onPressed: () {
+                          Scaffold.of(scaffoldContext).showSnackBar(SnackBar(content: Text("There's nothing to manage yet!")));
+                        }, child: Text("My Account"),),
+                      ],
+                    );
+                  }
+                }),
                 Divider(),
                 Center(child: Text("Change Your Colors", style: TextStyle(fontSize: 20),)),
                 Divider(),
@@ -108,6 +213,19 @@ class ScaffoldWrapperState extends State<ScaffoldWrapper> {
         },
       )
     );
+  }
+
+  void checkAuthentication() {
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      if (user != null) {
+        widget._user = user;
+      } else {
+        widget._user = null;
+      }
+    });
+    setState(() {
+
+    });
   }
 
 }
